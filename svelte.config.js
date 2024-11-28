ab7eaccd8431845aa04ad2ec66e842b85e62b1af
unknown <nancyaranda103@gmail.com>
2024-11-28 06:43:50 +0100

    Fixed adapter reference in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: vercel()
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
+
 export default config;
